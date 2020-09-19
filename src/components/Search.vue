9b975d951f0d9261d05582c6270b8a42ae34034e
Alejandro <alejandro.mercado@udea.edu.co>
2020-09-19 09:59:32 -0500

    Mi proyecto dojo vue

---
+++ src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-text-field label="Search" v-model="word"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn class="mx-2" fab dark color="blue lighten-34" @click = "searchItem">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
